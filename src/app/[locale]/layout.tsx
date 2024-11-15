0729cc79c52c5e07d0fb337e43aef1c8f71451dd
Azar Safarov <azar.s@xperience.lv>
2024-11-15 00:37:23 -0500

    multilanguage added

---
+++ src/app/[locale]/layout.tsx
@@ -0,0 +1,37 @@
+import { NextIntlClientProvider } from "next-intl";
+import { getMessages } from "next-intl/server";
+import { notFound } from "next/navigation";
+import { routing } from "@/i18n/routing";
+
+export async function generateStaticParams() {
+  return [{ locale: "en" }, { locale: "lv" }];
+}
+
+export default async function LocaleLayout({
+  children,
+  params,
+}: Readonly<{
+  children: React.ReactNode;
+  params: Promise<{ locale: string }>;
+}>) {
+  const { locale } = await params;
+
+  // Ensure that the incoming `locale` is valid
+  if (!routing.locales.includes(locale as "en" | "lv")) {
+    notFound();
+  }
+
+  // Providing all messages to the client
+  // side is the easiest way to get started
+  const messages = await getMessages();
+
+  return (
+    <html lang={locale}>
+      <body>
+        <NextIntlClientProvider messages={messages}>
+          {children}
+        </NextIntlClientProvider>
+      </body>
+    </html>
+  );
+}
